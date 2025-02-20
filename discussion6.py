3b4043988a4a2358401def540cf87226eeb046e2
Faris Khojah <fariskhojah@Fariss-MacBook-Air.local>
2025-02-20 11:30:40 -0500

    {2}

--- discussion6.py
+++ discussion6.py
@@ -1,10 +1,10 @@
 import unittest
 import os
-
+import csv
 
 def load_csv(f):
     '''
-    Params: 
+    Params:
         f, name or path or CSV file: string
 
     Returns:
@@ -12,12 +12,30 @@ def load_csv(f):
         outer keys are (str) years, values are dicts
         inner keys are (str) months, values are (str) integers
     
-    Note: Don't strip or otherwise modify strings. Don't change datatypes from strings. 
+    Note: Don't strip or otherwise modify strings. Don't change datatypes from strings.
     '''
-
+    
     base_path = os.path.abspath(os.path.dirname(__file__))
     full_path = os.path.join(base_path, f)
     # use this 'full_path' variable as the file that you open
+    with open(full_path) as fh:
+        r = csv.reader(fh)
+        rows = []
+        print(f"Add the data from the csv")
+        for row in r:
+            print(f"Adding {row} to rows")
+            rows.append(row)
+        print(f"Final value of rows is {rows}")
+    
+    print("Create a dictionary d")
+    d = {}
+    header = rows[0]
+    for year in header[1:]:
+        d[year] = rows[0]
+    print(f"Added the years, d is now {d}")
+    print("Add all the rows, skipping the first one")
+    
+    return d
 
 def get_annual_max(d):
     '''
@@ -25,24 +43,24 @@ def get_annual_max(d):
         d, dict created by load_csv above
 
     Returns:
-        list of tuples, each with 3 items: year (str), month (str), and max (int) 
+        list of tuples, each with 3 items: year (str), month (str), and max (int)
         max is the maximum value for a month in that year, month is the corresponding month
-
+    
     Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary.
-        You'll have to change vals to int to compare them. 
+        You'll have to change vals to int to compare them.
     '''
     pass
 
 def get_month_avg(d):
     '''
-    Params: 
+    Params:
         d, dict created by load_csv above
 
     Returns:
         dict where keys are years and vals are floats rounded to nearest whole num or int
         vals are the average vals for months in the year
-
-    Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary. 
+    
+    Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary.
         You'll have to make the vals int or float here and round the avg to pass tests.
     '''
     pass
@@ -55,14 +73,14 @@ class dis7_test(unittest.TestCase):
         self.flight_dict = load_csv('daily_visitors.csv')
         self.max_tup_list = get_annual_max(self.flight_dict)
         self.month_avg_dict = get_month_avg(self.flight_dict)
-
+    
     def test_load_csv(self):
         self.assertIsInstance(self.flight_dict['2021'], dict)
         self.assertEqual(self.flight_dict['2020']['JUN'], '435')
-
+    
     def test_get_annual_max(self):
         self.assertEqual(self.max_tup_list[2], ('2022', 'AUG', 628))
-
+    
     def test_month_avg_list(self):
         self.assertAlmostEqual(self.month_avg_dict['2020'], 398, 0)
 
